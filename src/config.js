a4603e77520172c8fa3d68f18be950d29ff9a5b3
ericsoft123 <ericsoft123@gmail.com>
2020-06-21 19:01:19 +0200

    Adding Key on config
    adding Keys

--- src/config.js
+++ src/config.js
@@ -1,18 +1,20 @@
 
+//Note for Testing Purpose i have decided to add key but let me know when you will be done i can remove key for security purpose
 let config;
 config={
     googleapi_url:"https://maps.googleapis.com/maps/api",
-    googleapi_key:"YOUR GOOGLE API KEY",
+    googleapi_key:"AIzaSyCSHiuycP-Ea9cdFKdfpGs0coDv2WjKAlw",
     foursquare_url:"https://api.foursquare.com/v2",
-    foursquare_clientid:"YOUR FOURSQUARE CLIENTID",
-    foursquare_clientsecret:"YOUR CLIENT SECRET",
-    foursquare_v:"VERSION",//IT COMES WITH clientid and client secret
+    foursquare_clientid:"KQ5QT5YMJ5NY4M1XX1ZVREBR0P10XC5A3GJHG0HDJIQ4Z3EY",
+    foursquare_clientsecret:"DNY5DQUWFDUMUYXL1AHDAGM14CGZQULWF3KY140BDJ30PNEA",
+    foursquare_v:"20190425",
 
     flickr_url:"https://api.flickr.com/services/rest/?method=",
-    flickr_key:"FLICKR_KEY",
+    flickr_key:"dd4a16666bdf3c2180b43bec8dd1534a",
     radius_val:10000,
     limit_val:10,
 }
 
 
 export {config};
+
