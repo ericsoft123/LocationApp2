c0a5ad02e97f7a63563749d5c6bf2f63326af69c
ericsoft123 <ericsoft123@gmail.com>
2020-07-29 15:30:18 +0200

    Update config.js
    just remove key

--- src/config.js
+++ src/config.js
@@ -3,14 +3,14 @@
 let config;
 config={
     googleapi_url:"https://maps.googleapis.com/maps/api",
-    googleapi_key:"AIzaSyCSHiuycP-Ea9cdFKdfpGs0coDv2WjKAlw",
+    googleapi_key:"googleapi",
     foursquare_url:"https://api.foursquare.com/v2",
-    foursquare_clientid:"KQ5QT5YMJ5NY4M1XX1ZVREBR0P10XC5A3GJHG0HDJIQ4Z3EY",
-    foursquare_clientsecret:"DNY5DQUWFDUMUYXL1AHDAGM14CGZQULWF3KY140BDJ30PNEA",
+    foursquare_clientid:"Flickr_clientid",
+    foursquare_clientsecret:"Flickr_clientsecrete",
     foursquare_v:"20190425",
 
     flickr_url:"https://api.flickr.com/services/rest/?method=",
-    flickr_key:"dd4a16666bdf3c2180b43bec8dd1534a",
+    flickr_key:"Flickr _key",
     radius_val:10000,
     limit_val:10,
 }
